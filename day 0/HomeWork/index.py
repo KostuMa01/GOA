7dbf2fffc4f0abe11affcf71c456ae0827a48e39
KostuMa01 <kostumakostuma11@gmail.com>
2024-06-09 18:46:45 +0400

    done

--- day 0/HomeWork/index.py
+++ day 0/HomeWork/index.py
@@ -1,16 +1,41 @@
 from turtle import *
 
-width(6)
-forward(150)
-left(90)
+penup()
+goto(-100, -100)
+pendown()
 
-forward(150)
-left(90)
+# Draw square
+speed(100)
+width(5)
+for _ in range(4):
+    forward(200)
+    left(90)
 
-forward(150)
+# Draw door
+begin_fill()
+forward(70)
+color("red")
 left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
 
-forward(150)
-left(90)
+#roof
+penup()
+goto(100, 100)
+pendown()
+
+begin_fill()
+color("blue")
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+#windows
 
 exitonclick()
